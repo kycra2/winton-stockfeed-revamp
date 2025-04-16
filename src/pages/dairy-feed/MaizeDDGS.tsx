cf4762345995ea5db5b21e99c485833e0ca5ecdb
kycra2 <craig@craigconnelly.co.nz>
2025-04-17 09:51:53 +1200

    Update MaizeDDGS.tsx

--- src/pages/dairy-feed/MaizeDDGS.tsx
+++ src/pages/dairy-feed/MaizeDDGS.tsx
@@ -62,7 +62,7 @@ const MaizeDDGSPage: React.FC = () => {
         <section className="space-y-2">
           <div className="rounded-lg overflow-hidden shadow-md">
             <img 
-              src="https://evp-5783e02c62446-6a755ff09bfda58c6e9423a7c237a863.s3.us-east-1.amazonaws.com/Screenshot+2025-04-17+at+9.41.48%E2%80%AFAM.png" 
+              src="https://github.com/kycra2/winton-stockfeed-revamp/blob/main/src/components/images/winton-stock-feed-auger-truck.jpg?raw=true" 
               alt="Maize DDGS Feed" 
               className="w-full h-auto object-cover"
             />
