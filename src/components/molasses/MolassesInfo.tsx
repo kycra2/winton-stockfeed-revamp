eaf8330c72a26e0abe81255b0ee8d4d6d6ba49c3
kycra2 <craig@craigconnelly.co.nz>
2025-04-16 12:07:33 +1200

    Update MolassesInfo.tsx

--- src/components/molasses/MolassesInfo.tsx
+++ src/components/molasses/MolassesInfo.tsx
@@ -22,7 +22,8 @@ const MolassesInfo = () => {
              Cane Molasses is a by-product of sugar refining – about 64% sugar on a dry matter basis.
 Molasses provides energy in the diet and has no protein so it is excellent as a pasture diet balancer improving feed conversion efficiency.
 Molasses has been a key supplement in the New Zealand pasture based dairy diets for over 30 years! 
-The perfect animal feed additive. Whether new to molasses or a serial user, Winton Stock Feed are the only New Zealand owned and independent importer.
+The perfect animal feed additive. Whether new to molasses or a serial user, Winton Stock Feed are the only New Zealand owned and independent importer. 
+We import using both "Container Surpluses" and bulk ships which means better shipping prices which we pass onto you our customers. 
             </p>
             <p className="mb-4 text-gray-700">
               At Winton StockFeed, we import high-quality molasses that provides essential minerals 
