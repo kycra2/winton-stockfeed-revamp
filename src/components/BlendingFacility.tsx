34a56c067e12e1a59d2b2d8ad69335bdd688ce6a
kycra2 <craig@craigconnelly.co.nz>
2025-04-23 08:49:58 +1200

    Update BlendingFacility.tsx

--- src/components/BlendingFacility.tsx
+++ src/components/BlendingFacility.tsx
@@ -5,7 +5,7 @@ const BlendingFacility = () => {
     <section className="bg-black text-white py-16">
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center gap-8">
         <img
-          src="https://cdn.convertri.com/2584a160-56c5-11e6-829d-066a9bd5fb79%2F9fd26178b1aff007abda9e961e238b07a6b74e89%2FScreenshot%202025-04-14%20at%202.35.49%E2%80%AFPM.png"
+          src="https://github.com/kycra2/winton-stockfeed-revamp/blob/main/src/components/images/stock-feed-blending-southland%20(1).jpg?raw=true"
           alt="Blending Facility"
           className="w-full md:w-1/2 rounded-lg"
         />
